f38b6ad1fabe9f403834b444ccc00c01291fbeb1
workwhileweb <workwhileweb@gmail.com>
2015-11-05 13:55:50 +0700

    add IronyFCTB
    add IronyFCTB (example of integration FCTB with parsing engine)

---
+++ IronyFCTB/Tester/SimplestSample.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Tester
+{
+    partial class SimplestSample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimplestSample));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.miLanguage = new System.Windows.Forms.ToolStripMenuItem();
+            this.cSharpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jsonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.csvToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gwBasicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.javaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miniPythonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.myCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schemeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sQLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wikiCodeplexToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ironyFCTB = new FastColoredTextBoxNS.IronyFCTB();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ironyFCTB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miLanguage});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(668, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // miLanguage
+            // 
+            this.miLanguage.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cSharpToolStripMenuItem,
+            this.jsonToolStripMenuItem,
+            this.csvToolStripMenuItem,
+            this.gwBasicToolStripMenuItem,
+            this.javaToolStripMenuItem,
+            this.miniPythonToolStripMenuItem,
+            this.myCToolStripMenuItem,
+            this.schemeToolStripMenuItem,
+            this.sQLToolStripMenuItem,
+            this.wikiCodeplexToolStripMenuItem});
+            this.miLanguage.Name = "miLanguage";
+            this.miLanguage.Size = new System.Drawing.Size(71, 20);
+            this.miLanguage.Text = "Language";
+            // 
+            // cSharpToolStripMenuItem
+            // 
+            this.cSharpToolStripMenuItem.Name = "cSharpToolStripMenuItem";
+            this.cSharpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cSharpToolStripMenuItem.Text = "CSharp";
+            this.cSharpToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // jsonToolStripMenuItem
+            // 
+            this.jsonToolStripMenuItem.Name = "jsonToolStripMenuItem";
+            this.jsonToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.jsonToolStripMenuItem.Text = "Json";
+            this.jsonToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // csvToolStripMenuItem
+            // 
+            this.csvToolStripMenuItem.Name = "csvToolStripMenuItem";
+            this.csvToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.csvToolStripMenuItem.Text = "Csv";
+            this.csvToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // gwBasicToolStripMenuItem
+            // 
+            this.gwBasicToolStripMenuItem.Name = "gwBasicToolStripMenuItem";
+            this.gwBasicToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.gwBasicToolStripMenuItem.Text = "GwBasic";
+            this.gwBasicToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // javaToolStripMenuItem
+            // 
+            this.javaToolStripMenuItem.Name = "javaToolStripMenuItem";
+            this.javaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.javaToolStripMenuItem.Text = "Java";
+            this.javaToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // miniPythonToolStripMenuItem
+            // 
+            this.miniPythonToolStripMenuItem.Name = "miniPythonToolStripMenuItem";
+            this.miniPythonToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.miniPythonToolStripMenuItem.Text = "MiniPython";
+            this.miniPythonToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // myCToolStripMenuItem
+            // 
+            this.myCToolStripMenuItem.Name = "myCToolStripMenuItem";
+            this.myCToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.myCToolStripMenuItem.Text = "My C";
+            this.myCToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // schemeToolStripMenuItem
+            // 
+            this.schemeToolStripMenuItem.Name = "schemeToolStripMenuItem";
+            this.schemeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.schemeToolStripMenuItem.Text = "Scheme";
+            this.schemeToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // sQLToolStripMenuItem
+            // 
+            this.sQLToolStripMenuItem.Name = "sQLToolStripMenuItem";
+            this.sQLToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.sQLToolStripMenuItem.Text = "SQL";
+            this.sQLToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // wikiCodeplexToolStripMenuItem
+            // 
+            this.wikiCodeplexToolStripMenuItem.Name = "wikiCodeplexToolStripMenuItem";
+            this.wikiCodeplexToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.wikiCodeplexToolStripMenuItem.Text = "Wiki-Codeplex";
+            this.wikiCodeplexToolStripMenuItem.Click += new System.EventHandler(this.miClick);
+            // 
+            // ironyFCTB
+            // 
+            this.ironyFCTB.AutoScrollMinSize = new System.Drawing.Size(669, 645);
+            this.ironyFCTB.BackBrush = null;
+            this.ironyFCTB.CharHeight = 15;
+            this.ironyFCTB.CharWidth = 7;
+            this.ironyFCTB.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ironyFCTB.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.ironyFCTB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ironyFCTB.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ironyFCTB.IsReplaceMode = false;
+            this.ironyFCTB.Location = new System.Drawing.Point(0, 24);
+            this.ironyFCTB.Name = "ironyFCTB";
+            this.ironyFCTB.Paddings = new System.Windows.Forms.Padding(0);
+            this.ironyFCTB.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.ironyFCTB.Size = new System.Drawing.Size(668, 380);
+            this.ironyFCTB.TabIndex = 2;
+            this.ironyFCTB.Text = resources.GetString("ironyFCTB.Text");
+            this.ironyFCTB.Zoom = 100;
+            // 
+            // SimplestSample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 404);
+            this.Controls.Add(this.ironyFCTB);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SimplestSample";
+            this.Text = "SimplestSample";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ironyFCTB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem miLanguage;
+        private System.Windows.Forms.ToolStripMenuItem cSharpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jsonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem csvToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gwBasicToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem javaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miniPythonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem myCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schemeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sQLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wikiCodeplexToolStripMenuItem;
+        private FastColoredTextBoxNS.IronyFCTB ironyFCTB;
+    }
+}
---
+++ IronyFCTB/Tester/SimplestSample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using Demo;
+using Irony.Samples;
+using Irony.Samples.CSharp;
+using Irony.Samples.Java;
+using Irony.Samples.Json;
+using Irony.Samples.MiniPython;
+using Irony.Samples.SQL;
+using Irony.Samples.Scheme;
+
+namespace Tester
+{
+    public partial class SimplestSample : Form
+    {
+        public SimplestSample()
+        {
+            InitializeComponent();
+            cSharpToolStripMenuItem.PerformClick();
+        }
+
+        private void miClick(object sender, EventArgs e)
+        {
+            var langName = sender.ToString();
+            miLanguage.Text = langName;
+            switch(langName)
+            {
+                case "CSharp": ironyFCTB.Grammar = new CSharpGrammar(); break;
+                case "Json": ironyFCTB.Grammar = new JsonGrammar(); break;
+                case "Csv": ironyFCTB.Grammar = new SampleCsvGrammar(); break;
+                case "GwBasic": ironyFCTB.Grammar = new GWBasicGrammar(); break;
+                case "Java": ironyFCTB.Grammar = new JavaGrammar(); break;
+                case "MiniPython": ironyFCTB.Grammar = new MiniPythonGrammar(); break;
+                case "My C": ironyFCTB.Grammar = new MyCGrammar(); break;
+                case "Scheme": ironyFCTB.Grammar = new SchemeGrammar(); break;
+                case "SQL": ironyFCTB.Grammar = new SqlGrammar(); break;
+                case "Wiki-Codeplex": ironyFCTB.Grammar = new WikiCodeplexGrammar(); break;
+            }
+        }
+    }
+}
